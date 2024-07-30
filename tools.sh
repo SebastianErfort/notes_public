5736a18d81439fe58acc336004b87533d1b7ae46
Sebastian Erfort <s.erfort@rug.nl>
2024-07-30 17:56:42 +0200

    upd tools (wip): working on POST request to trigger gh pages pipeline

--- tools.sh
+++ tools.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# A collection of tools for handling notes
+# - Synchronisation with another repository
+# - Markdown tools: see ~/rsrc/markdown/markdown.sh
 
-function sync_fmkey () {
+function sync_fmkey() {
     # TODO ensure fm exists, else yq fails
     [[ $# != 2 ]] && echo "Usage: <key> <value>" && return 2
     local key=${1:-visibility} value=${2:-public}
@@ -8,14 +11,14 @@ function sync_fmkey () {
 
     mapfile -t syncfiles < <(get_files_fmmatch "$key" "$value")
 
-    for (( i=0; i<${#syncfiles[@]} ; i++ )); do
+    for ((i = 0; i < ${#syncfiles[@]}; i++)); do
         f=${syncfiles[$i]}
         # NOTE: yq didn't like double ticks :sigh:
         yq -i -f process '.'"${key}=\"${value}\"" "$f"
     done
 }
 
-function get_files_fmmatch () {
+function get_files_fmmatch() {
     local msg_usage="Usage: ${FUNCNAME[0]} <key> <value>"
     [[ $# != 2 ]] && echo "$msg_usage" && return 2
     local -a allfiles files
@@ -28,7 +31,7 @@ function get_files_fmmatch () {
     printf "%s\n" "${files[@]}"
 }
 
-function push_fmmatch () {
+function push_fmmatch() {
     # Push all files where YAML frontmatter <key> has <value> to <target dir>
     # #markdown #markdown/frontmatter
     # TODO: Would be cooler with Git/version control than rsync
@@ -46,7 +49,7 @@ function push_fmmatch () {
     if [[ -f public.txt ]]; then
         while read -r l; do
             syncfiles+=("$l")
-        done < public.txt
+        done <public.txt
     fi
     # sort and unique
     mapfile -t syncfiles < <(printf '%s\n' "${syncfiles[@]}" | sort -u)
@@ -56,12 +59,46 @@ function push_fmmatch () {
     echo "Done."
 }
 
-function pull_fmmatch () {
-    [[ $# != 1 ]] && echo "Usage: <source dir>" && return 2
+function pull_fmmatch() {
+    [[ $# != 1 ]] &&
+        {
+            echo "Usage: <source dir>"
+            return 2
+        }
 
-    echo "Copying files from $1 ..."
-    cp -ar "$1"/* .
-    # rsync -aLR "$3/*" .
+    echo "Copying files from $3 ..."
+    rsync -aLR "$1/*" .
 
     echo "Done."
 }
+
+gh_dispatch() {
+    # WARNING: Not yet functional (2024-07-30): 403 error "Resource not
+    # accessible by personal access token"
+    [[ -z "$GH_USER" ]] && GH_USER="$(git config --global --get user.name)"
+    GH_OWNER="$(git config --no-global remote.origin.url | sed 's/^.*://;s/\.git$//' | awk -F/ '{print $1}')"
+    GH_REPO="$(git config --no-global remote.origin.url | sed 's/^.*://;s/\.git$//' | awk -F/ '{print $2}')"
+    DISPATCH_TYPE=updated_source
+    # echo "Triggering workflow for user ${GH_USER} and repo ${GH_REPO} with token ${ACCESS_TOKEN}"
+    curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: token ${ACCESS_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/${GH_OWNER}/${GH_REPO}/dispatches" \
+        -d '{"event_type":"updated_source","client_payload":{"unit":false,"integration":true}}'
+}
+
+if [[ "$0" != "${BASH_SOURCE[0]}" ]]; then
+    [[ -f "$HOME/rsrc/markdown/markdown.sh" ]] && . "$HOME/rsrc/markdown/markdown.sh"
+else
+    case "$1" in
+    cicd | pipeline | trigger)
+        gh_dispatch
+        ;;
+    *)
+        echo "Unknown command ${1}"
+        return 2
+        ;;
+    esac
+fi
